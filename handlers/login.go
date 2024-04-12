462d20547866e105c68057dc25eac8eabcdaf28c
b-munar <b.munar@uniandes.edu.co>
2024-04-12 14:21:12 -0500

    feat: register ok, login in process

--- handlers/login.go
+++ handlers/login.go
@@ -34,13 +34,30 @@ func Login(c *fiber.Ctx) error {
 			return c.Status(400).JSON(fiber.Map{"status": "failure"})
 		}
 
-		var SubJson structs.Subscription
-		err_3 := json.Unmarshal(response_sub.Data, &SubJson)
+		type Activate struct {
+			Activate bool `json:"activate"`
+		}
+
+		type SubscriptionActivate struct {
+			Subscription Activate `json:"subscription"`
+		}
+
+		var SubAct SubscriptionActivate
+
+		err_3 := json.Unmarshal(response_sub.Data, &SubAct)
 		if err_3 != nil {
 			return c.Status(400).JSON(fiber.Map{"status": "failure"})
 		}
-		return c.Status(202).JSON(fiber.Map{"sportmen": SportmenJson.Sportmen, "auth": UserJson.Auth, "subscription": SubJson.Sub})
 
+		if SubAct.Subscription.Activate == false {
+			var SubJson structs.SubscriptionList
+			err_4 := json.Unmarshal(response_sub.Data, &SubJson)
+			if err_4 != nil {
+				return c.Status(400).JSON(fiber.Map{"status": "failure"})
+			}
+			return c.Status(202).JSON(fiber.Map{"sportmen": SportmenJson.Sportmen, "auth": UserJson.Auth, "subscription": SubJson.Subscriptions})
+
+		}
 	}
 	if UserJson.Auth.Role == 2 {
 
